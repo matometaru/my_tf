96966a821a8ada1f6a74262f524a179258b712ca
kita <r.kita@remodela.jp>
2024-07-08 21:51:53 +0900

    C_5_2_1

---
+++ packages/practice5/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import C_5_2_1 from './components/C_5_2_1';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <C_5_2_1 />
+    </>
+  )
+}
+
+export default App;
---
+++ packages/practice5/src/components/C_5_2_1.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import * as tf from '@tensorflow/tfjs';
+
+function C_4_5_1() {
+  useEffect(() => {
+  }, []);
+
+  return (
+    <div>
+      <p>C_5_2_1</p>
+    </div>
+  );
+}
+
+export default C_4_5_1;
